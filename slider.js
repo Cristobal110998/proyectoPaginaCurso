5e2b8251e8bee465f4e52a93179cbeaadaed7b58
Cristobal110998 <cristobal.barajas@alumnos.udg.mx>
2021-09-01 15:38:09 -0500

    cambios_formato

--- slider.js
+++ slider.js
@@ -1,20 +1,20 @@
-var slides = document.querySelectorAll('.slide');
-var btns = document.querySelectorAll('.btn');
+var slides = document.querySelectorAll(".slide");
+var btns = document.querySelectorAll(".btn");
 let currentSlide = 1;
 
 // Javascript for image slider manual navigation
-var manualNav = function(manual){
+var manualNav = function (manual) {
   slides.forEach((slide) => {
-    slide.classList.remove('active');
+    slide.classList.remove("active");
 
     btns.forEach((btn) => {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
     });
   });
 
-  slides[manual].classList.add('active');
-  btns[manual].classList.add('active');
-}
+  slides[manual].classList.add("active");
+  btns[manual].classList.add("active");
+};
 
 btns.forEach((btn, i) => {
   btn.addEventListener("click", () => {
@@ -24,68 +24,74 @@ btns.forEach((btn, i) => {
 });
 
 // Javascript for image slider autoplay navigation
-var repeat = function(activeClass){
-  let active = document.getElementsByClassName('active');
+var repeat = function (activeClass) {
+  let active = document.getElementsByClassName("active");
   let i = 1;
 
   var repeater = () => {
-    setTimeout(function(){
+    setTimeout(function () {
       [...active].forEach((activeSlide) => {
-        activeSlide.classList.remove('active');
+        activeSlide.classList.remove("active");
       });
 
-    slides[i].classList.add('active');
-    btns[i].classList.add('active');
-    i++;
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      i++;
 
-    if(slides.length == i){
-      i = 0;
-    }
-    if(i >= slides.length){
-      return;
-    }
-    repeater();
-  }, 5000);
-  }
+      if (slides.length == i) {
+        i = 0;
+      }
+      if (i >= slides.length) {
+        return;
+      }
+      repeater();
+    }, 5000);
+  };
   repeater();
-}
+};
 repeat();
 
-function test(){
-  var tabsNewAnim = $('#navbarSupportedContent');
-  var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-  var activeItemNewAnim = tabsNewAnim.find('.active');
+function test() {
+  var tabsNewAnim = $("#navbarSupportedContent");
+  var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
+  var activeItemNewAnim = tabsNewAnim.find(".active");
   var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
   var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
   var itemPosNewAnimTop = activeItemNewAnim.position();
   var itemPosNewAnimLeft = activeItemNewAnim.position();
   $(".hori-selector").css({
-    "top":itemPosNewAnimTop.top + "px", 
-    "left":itemPosNewAnimLeft.left + "px",
-    "height": activeWidthNewAnimHeight + "px",
-    "width": activeWidthNewAnimWidth + "px"
+    top: itemPosNewAnimTop.top + "px",
+    left: itemPosNewAnimLeft.left + "px",
+    height: activeWidthNewAnimHeight + "px",
+    width: activeWidthNewAnimWidth + "px",
   });
-  $("#navbarSupportedContent").on("click","li",function(e){
-    $('#navbarSupportedContent ul li').removeClass("active");
-    $(this).addClass('active');
+  $("#navbarSupportedContent").on("click", "li", function (e) {
+    $("#navbarSupportedContent ul li").removeClass("active");
+    $(this).addClass("active");
     var activeWidthNewAnimHeight = $(this).innerHeight();
     var activeWidthNewAnimWidth = $(this).innerWidth();
     var itemPosNewAnimTop = $(this).position();
     var itemPosNewAnimLeft = $(this).position();
     $(".hori-selector").css({
-      "top":itemPosNewAnimTop.top + "px", 
-      "left":itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
+      top: itemPosNewAnimTop.top + "px",
+      left: itemPosNewAnimLeft.left + "px",
+      height: activeWidthNewAnimHeight + "px",
+      width: activeWidthNewAnimWidth + "px",
     });
   });
 }
-$(document).ready(function(){
-  setTimeout(function(){ test(); });
+$(document).ready(function () {
+  setTimeout(function () {
+    test();
+  });
 });
-$(window).on('resize', function(){
-  setTimeout(function(){ test(); }, 500);
+$(window).on("resize", function () {
+  setTimeout(function () {
+    test();
+  }, 500);
 });
-$(".navbar-toggler").click(function(){
-  setTimeout(function(){ test(); });
+$(".navbar-toggler").click(function () {
+  setTimeout(function () {
+    test();
+  });
 });
